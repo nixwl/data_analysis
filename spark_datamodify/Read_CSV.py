2fccaff419e7af56db192c6ea60ec2a6ea7380ff
nixwl <80773211+nixwl@users.noreply.github.com>
2023-07-15 16:55:48 +0800

    modified Read_CSV.py

--- spark_datamodify/Read_CSV.py
+++ spark_datamodify/Read_CSV.py
@@ -3,7 +3,10 @@ from pyspark.sql import SQLContext
 
 spark = SparkSession.builder.\
     appName("read_csv").\
+    master("local").\
     getOrCreate()
+    # master("spark://node-1:7077").\
+
 
 df = spark.read.format('csv')\
     .option("sep", ",")\
@@ -12,7 +15,49 @@ df = spark.read.format('csv')\
     .schema("userId STRING, movieId STRING, rating DOUBLE, tstamp TIMESTAMP" )\
     .load('hdfs://node-1:9000/user/root/testfile/ratings.csv')
 
+# 筛选 without groups (shorthand for df.groupBy().agg()).
+# df.agg({"rating": "max"}).show()
 
-df.printSchema()
-df.show()
+# DataFrame 别名
+# df_alias = df.alias("df_alias")
+# df_alias.show()
 
+# DataFrame‘s Column's approxQuantile 近似分位数
+# list_ = df.approxQuantile('rating', (0.25, ), 0.01)
+# print(list_)
+
+# 持久化设定：pyspark.sql.DataFrame.Cache     pyspark.sql.DataFrame.checkpoint
+# 重新划定分区：pyspark.sql.DataFrame.coalesce
+
+# pyspark.sql.DataFrame.colRegex
+# Selects column based on the column name specified as a regex and returns it as Column.
+
+# Returns all the records as a list of Row
+# df.collect()
+
+# 输出表架构
+# df.printSchema()
+# 打印表
+# df.show()
+# df.count()
+# df2 = df.limit(5)
+#d f2.count()
+# df2.show()
+### write to mysql
+# df2.write.mode("overwrite").\
+#      format("jdbc").\
+#      option("url","jdbc:mysql://192.168.101.20:3306/spark?useSSL=false&Unicode=true").\
+#      option("dbtable","movie_data_1").\
+#      option("user","spark").\
+#      option("password","12345678").\
+#      save()
+
+
+df3 = spark.read.format("jdbc").\
+     option("url","jdbc:mysql://192.168.101.20:3306/spark?useSSL=false&Unicode=true").\
+     option("dbtable","movie_data_1").\
+     option("user","spark").\
+     option("password","12345678").\
+     load()
+
+df3.show()
